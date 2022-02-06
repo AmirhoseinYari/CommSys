49eea85fac1edbb145bc96312db41db354e3149e
AmirhoseinYari <amirhoseinyari@icloud.com>
2022-02-06 15:05:54 +0330

    Section 1-4

---
+++ Codes/Channel.m
@@ -0,0 +1,22 @@
+function y = Channel(xc, fs, fc, BW)
+y = bandpass(xc,[fc-BW/2 fc+BW/2],fs);
+Xc = fftshift(fft(xc));
+Y = fftshift(fft(y));
+
+figure
+N = length(xc);
+f =  (-N/2:N/2-1)*(fs/N);
+fmax = 2000;
+n = N*fmax/fs;
+subplot(2,1,1)
+plot(f(N/2-n:N/2+n),abs(Xc(N/2-n:N/2+n)))
+title('Received signal Fourier transform :')
+ylabel('Received(f)')
+xlabel('frequency')
+
+subplot(2,1,2)
+plot(f(N/2-n:N/2+n),abs(Y(N/2-n:N/2+n)))
+title('Filtered Received signal Fourier transform :')
+ylabel('Filtered-Received(f)')
+xlabel('frequency')
+end
--- Codes/Q1.m
+++ Codes/Q1.m
@@ -2,22 +2,25 @@ clc
 clear all
 close all
 
-b = [1 0 1 0 0 1]
-[b1,b2] = Divide(b)
+b = randi([0 1],1,1000);
+[b1,b2] = Divide(b);
 fprintf('stopped, press Enter');
 pause;
 
-Combine(b1,b2)
+Combine(b1,b2);
 fprintf('stopped, press Enter');
 pause;
 
 Shape = [0 1 3 1 0];
-x1 = PulseShaping(b1,Shape,-Shape)
-x2 = PulseShaping(b2,Shape,-Shape)
+x1 = PulseShaping(b1,Shape,-Shape);
+x2 = PulseShaping(b2,Shape,-Shape);
 fprintf('stopped, press Enter');
 pause;
 
-fs = 1000000;
-fc = 1000;% hshdijjjjsdjpf
-xc = AnalogMod(x1, x2, fs, fc) % hhgjhgkjgj
+fs = 10000;
+fc = 1000;
+xc = AnalogMod(x1, x2, fs, fc)
+fprintf('stopped, press Enter');
+pause;
 
+y = Channel(xc, fs, fc, 200);
