c91e969690a46821f421511f16187d6dde7da206
AmirhoseinYari <amirhoseinyari@icloud.com>
2022-02-07 19:37:42 +0330

    Part 1.5 Analog Demodulation

--- Codes/not/Combine.m
+++
@@ -1,5 +0,0 @@
-function b = Combine(b1,b2)
-N = length(b1)+length(b2);
-b(1:2:N-1) = b1;
-b(2:2:N) = b2;
-end
--- Codes/not/Divide.m
+++
@@ -1,5 +0,0 @@
-function [b1, b2] = Divide(b)
-N = length(b);
-b1 = b(1:2:N-1);
-b2 = b(2:2:N);
-end
